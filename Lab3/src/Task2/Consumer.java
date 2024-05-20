e24ebd3b29f03d2be7420259b1edd59900d95453
maksb <bondax28@gmail.com>
2024-05-20 21:40:32 +0300

    Revert "2 lab striped done"
    This reverts commit 6377587352ded09df72e162e8ed33bb995ac9290.

--- Lab3/src/Task2/Consumer.java
+++ Lab3/src/Task2/Consumer.java
@@ -11,17 +11,13 @@ public class Consumer implements Runnable {
 
     public void run() {
         Random random = new Random();
-        int count = 0;
         for (int message = drop.take();
              message != 0;
              message = drop.take()) {
-
             System.out.format("MESSAGE RECEIVED: %s%n", message);
-            count++;
             try {
                 Thread.sleep(6);
             } catch (InterruptedException e) {}
         }
-        System.out.format("Total messages received: %s%n", count);
     }
 }
