dfb732e72c7cd5f7351ef0be0eca80cb61ceeb90
maksb <bondax28@gmail.com>
2024-06-04 11:35:08 +0300

    lab 4 books edit

--- Lab4/src/task1/Main.java
+++ Lab4/src/task1/Main.java
@@ -7,15 +7,17 @@ import java.util.concurrent.ForkJoinPool;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String directoryPath = "C:\\Users\\maksb\\IdeaProjects\\parallel labs\\Lab4\\src\\books";
+        String directoryPath = "C:\\Users\\maksb\\IdeaProjects\\parallel labs\\Lab4\\src\\books\\TEST2";
 
         // warm up
+        System.out.println("Warmup started...");
         List<String> wordsWarmUp = FileProcessor.readFilesFromDirectory(directoryPath);
         WordLengthTask warmUpTask = new WordLengthTask(wordsWarmUp);
         ForkJoinPool warmUpPool = new ForkJoinPool(6);
         warmUpPool.invoke(warmUpTask);
         List<String> wordsSeq = FileProcessor.readFilesFromDirectorySequential(directoryPath);
         SequentialWordLengthCalculator.calculateAverageWordLength(wordsSeq);
+        System.out.println("Warmup finished.");
 
 
 
@@ -24,9 +26,12 @@ public class Main {
         List<String> wordsParallel = FileProcessor.readFilesFromDirectory(directoryPath);
         long endParallelRead = System.currentTimeMillis();
 
+        System.out.println("Amount of words in files: " + wordsParallel.size());
+        System.out.println("Amount of files: " + FileProcessor.getFilePaths(directoryPath).size());
+
         // parallel calculate
         long startParallelCalc = System.currentTimeMillis();
-        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        ForkJoinPool forkJoinPool = new ForkJoinPool(6);
         WordLengthTask parallelTask = new WordLengthTask(wordsParallel);
         WordLengthResult parallelResult = forkJoinPool.invoke(parallelTask);
         double parallelAverage = (double) parallelResult.totalLength / parallelResult.wordCount;
